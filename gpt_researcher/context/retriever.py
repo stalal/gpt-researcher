2b10e5fec20a7619f1da1b32211bd3106f3d0811
Talal Syed <3298457+stalal@users.noreply.github.com>
2025-11-01 17:56:48 -0400

    Update retriever.py

--- gpt_researcher/context/retriever.py
+++ gpt_researcher/context/retriever.py
@@ -3,8 +3,8 @@ from enum import Enum
 from typing import Any, Dict, List, Optional
 
 from langchain_core.callbacks.manager import CallbackManagerForRetrieverRun
-from langchain.schema import Document
-from langchain.schema.retriever import BaseRetriever
+from langchain_core.documents import Document
+from langchain_core.retrievers import BaseRetriever
 
 
 class SearchAPIRetriever(BaseRetriever):
